00cf95f85fbe11555aa9d8cb2992494b2da66512
Alistair A. Israel <aisrael@gmail.com>
2020-03-25 16:35:23 +0800

    Crockford Base32 encoding/decoding works :D

--- lib/utils/base32_crockford.dart
+++ lib/utils/base32_crockford.dart
@@ -1,22 +1,97 @@
 import 'dart:convert';
 
+/// Character '9'
+const $9 = 0x39;
+
+/// A CrockfordCodec singleton
 final crockford = CrockfordCodec();
+
+const normalizationMap = {
+  // 'I': '1'
+  73: 49,
+  // 'L': '1'
+  76: 49,
+  // 'i': '1'
+  105: 49,
+  // 'l': '1'
+  108: 49,
+  // 'O': '0'
+  79: 48,
+  // 'o': '0'
+  111: 48,
+};
+
+String normalize(final String input) {
+  final codeUnits = input.toUpperCase().codeUnits;
+  final List<int> out = List(codeUnits.length);
+  for (var i = 0; i < codeUnits.length; ++i) {
+    final inp = codeUnits[i];
+    out[i] = normalizationMap.containsKey(inp) ? normalizationMap[inp] : inp;
+  }
+  return AsciiDecoder().convert(out);
+}
 
 class CrockfordCodec extends Codec<int, String> {
   @override
-  // TODO: implement decoder
-  Converter<String, int> get decoder => null;
+  Converter<String, int> get decoder => CrockfordDecoder();
 
   @override
   Converter<int, String> get encoder => CrockfordEncoder();
 }
 
-const encoding = '0123456789ABCDEFGHJKMNPQRSTVWXYZ';
+const alphaEncodingMap = {
+  100: 68,
+  101: 69,
+  102: 70,
+  103: 71,
+  104: 72,
+  105: 74,
+  106: 75,
+  107: 77,
+  108: 78,
+  109: 80,
+  110: 81,
+  111: 82,
+  112: 83,
+  113: 84,
+  114: 86,
+  115: 87,
+  116: 88,
+  117: 89,
+  118: 90,
+};
 
 class CrockfordEncoder extends Converter<int, String> {
   @override
   String convert(int input) {
-    // TODO: implement convert
-    return '';
+    final rawBase32 = input.toRadixString(32);
+    final codeUnits = rawBase32.codeUnits;
+    final List<int> out = List(codeUnits.length);
+    for (var i = 0; i < codeUnits.length; ++i) {
+      final inp = codeUnits[i];
+      out[i] = inp <= $9 ? inp : alphaEncodingMap[codeUnits[i]];
+    }
+    return AsciiDecoder().convert(out);
+  }
+}
+
+final symbols = '0123456789ABCDEFGHJKMNPQRSTVWXYZ'.codeUnits;
+
+class CrockfordDecoder extends Converter<String, int> {
+  @override
+  int convert(String input) {
+    final codeUnits = normalize(input).codeUnits;
+    var n = 0;
+    for (int i = 0; i < codeUnits.length; ++i) {
+      final index = symbols.indexOf(codeUnits[i]);
+      if (index == -1) {
+        throw new FormatException(
+            "Invalid character encountered: '${input.substring(i, i + 1)}'",
+            input,
+            i);
+      }
+      n = (n << 5) + index;
+    }
+    return n;
   }
 }
--- test/utils/base32_crockford_test.dart
+++ test/utils/base32_crockford_test.dart
@@ -3,6 +3,31 @@ import 'package:test/test.dart';
 
 void main() {
   test('Base32Crockford.encode(int) works', () {
-    final String encoded = crockford.encode(1);
+    final String encoded = crockford.encode(987654321);
+    expect(encoded, equals('XDWT5H'));
+  });
+  test('normalize() works', () {
+    final testCases = {
+      '': '',
+      '0': '0',
+      '0123456789abcdefgHJKMNPQRSTUVwxyz': '0123456789ABCDEFGHJKMNPQRSTUVWXYZ',
+      'ILilOo': '111100'
+    };
+    testCases.forEach((input, expected) {
+      expect(normalize(input), equals(expected));
+    });
+  });
+  test('Base32Crockford.decode(String) works', () {
+    final testCases = {
+      '': 0,
+      '0': 0,
+      'Z': 31,
+      '10': 32,
+      'abc091': int.parse('abc091', radix: 32),
+      'ILilOo': int.parse('111100', radix: 32)
+    };
+    testCases.forEach((input, expected) {
+      expect(crockford.decode(input), equals(expected));
+    });
   });
 }
