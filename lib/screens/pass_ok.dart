d1820f12356cf9fdd50400b6dd57c0f68a939f81
Alistair A. Israel <aisrael@gmail.com>
2020-03-27 14:34:21 +0800

    Ok this is fine for now

--- lib/screens/pass_ok.dart
+++ lib/screens/pass_ok.dart
@@ -3,6 +3,44 @@ import 'package:flutter/material.dart';
 import 'package:rapidpass_checkpoint/models/qr_data.dart';
 import 'package:rapidpass_checkpoint/themes/default.dart';
 
+/// TODO: Come up with a better name
+class BoxWithRoundedBordersAndFilledHeader extends StatelessWidget {
+  final Widget header;
+  final Widget body;
+  final Color color;
+  BoxWithRoundedBordersAndFilledHeader(
+      {Widget header, Widget body, Color color})
+      : this.header = header ?? Container(),
+        this.body = body ?? Container(),
+        this.color = color ?? green300;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(
+            border: Border.all(color: this.color, width: 1.0),
+            borderRadius: BorderRadius.circular(8.0)),
+        margin: const EdgeInsets.symmetric(vertical: 10.0),
+        child: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+              Container(
+                color: green300,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: this.header,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                child: this.body,
+              )
+            ])));
+  }
+}
+
 class PassOkScreen extends StatelessWidget {
   QrData qrData;
 
@@ -23,45 +61,33 @@ class PassOkScreen extends StatelessWidget {
       child: Scaffold(
           appBar: AppBar(title: Text('Result')),
           body: Padding(
-            padding:
-                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-            child: Container(
-                decoration: BoxDecoration(
-                    border: Border.all(color: green300, width: 1.0),
-                    borderRadius: BorderRadius.circular(8.0)),
-                margin: const EdgeInsets.symmetric(vertical: 10.0),
-                child: Center(
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    BoxWithRoundedBordersAndFilledHeader(
+                      header: Column(
                         children: <Widget>[
-                      Container(
-                        color: green300,
-                        child: Padding(
-                          padding: const EdgeInsets.all(20.0),
-                          child: Center(
-                            child: Column(
-                              children: <Widget>[
-                                Padding(
-                                  padding: const EdgeInsets.all(10.0),
-                                  child: Image(
-                                    width: 80.0,
-                                    image: AssetImage('assets/check-2x.png'),
-                                  ),
-                                ),
-                                Text(
-                                  'ENTRY APPROVED',
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 22.0,
-                                      fontWeight: FontWeight.bold),
-                                )
-                              ],
+                          Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Image(
+                              width: 80.0,
+                              image: AssetImage('assets/check-2x.png'),
                             ),
                           ),
-                        ),
+                          Text(
+                            'ENTRY APPROVED',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 22.0,
+                                fontWeight: FontWeight.bold),
+                          )
+                        ],
                       ),
-                      Padding(
+                      body: Padding(
                           padding: const EdgeInsets.all(10.0),
                           child: Table(
                               children: tableData.entries
@@ -78,49 +104,51 @@ class PassOkScreen extends StatelessWidget {
                                         )
                                       ]))
                                   .toList())),
-                      Spacer(),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 10.0, horizontal: 20.0),
-                        child: SizedBox(
-                          height: 48,
-                          width: 300.0,
-                          child: RaisedButton(
-                            shape: new RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(24.0)),
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            child: Text('Scan another QR code',
-                                style: TextStyle(
-                                    // Not sure how to get rid of color: Colors.white here
-                                    color: Colors.white,
-                                    fontSize: 16.0)),
-                          ),
+                    ),
+                    Spacer(),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 20.0),
+                      child: SizedBox(
+                        height: 48,
+                        width: 300.0,
+                        child: RaisedButton(
+                          shape: new RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(24.0)),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text('Scan another QR code',
+                              style: TextStyle(
+                                  // Not sure how to get rid of color: Colors.white here
+                                  color: Colors.white,
+                                  fontSize: 16.0)),
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 10.0, horizontal: 20.0),
-                        child: SizedBox(
-                          height: 48,
-                          child: FlatButton(
-                            shape: new RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(24.0),
-                                side: BorderSide(color: green300)),
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            child: Text('Return to checker page',
-                                style: TextStyle(
-                                    // Not sure how to get rid of color: Colors.white here
-                                    color: green300,
-                                    fontSize: 16.0)),
-                          ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 20.0),
+                      child: SizedBox(
+                        height: 48,
+                        child: FlatButton(
+                          shape: new RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(24.0),
+                              side: BorderSide(color: green300)),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text('Return to checker page',
+                              style: TextStyle(
+                                  // Not sure how to get rid of color: Colors.white here
+                                  color: green300,
+                                  fontSize: 16.0)),
                         ),
-                      )
-                    ]))),
-          )),
+                      ),
+                    )
+                  ],
+                ),
+              ))),
     );
   }
 }
--- lib/themes/default.dart
+++ lib/themes/default.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
-final Color deepPurple300 = Color(0xff9575CD);
-final Color deepPurple600 = Color(0xff5E35B1);
-final Color deepPurple900 = Color(0xff311B92);
-final Color green300 = Color(0xff81C784);
+final Color deepPurple300 = const Color(0xff9575CD);
+final Color deepPurple600 = const Color(0xff5E35B1);
+final Color deepPurple900 = const Color(0xff311B92);
+final Color green300 = const Color(0xff81C784);
 
 final ColorScheme light = ColorScheme.light();
 final ColorScheme rapidPassColorScheme = ColorScheme(
