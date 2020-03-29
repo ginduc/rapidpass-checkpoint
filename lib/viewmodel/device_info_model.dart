b4f40603f0b8cbbd78c4c39f91dac4cb29f9504b
jdeguzman <joshua.maiquez.deguzman@gmail.com>
2020-03-29 15:30:53 +0800

    Fix imei retrieval

--- lib/viewmodel/device_info_model.dart
+++ lib/viewmodel/device_info_model.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:device_id/device_id.dart';
+import 'package:imei_plugin/imei_plugin.dart';
 
 class DeviceInfoModel extends ChangeNotifier {
   String _imei;
   String _deviceId;
 
   Future<String> getImei() async {
-    _imei = await DeviceId.getIMEI;
+    _imei =
+        await ImeiPlugin.getImei();
     debugPrint('imei => $_imei');
     return _imei;
   }
 
-  Future<String> getDeviceId() async {
+  Future<String> getDeviceId() async {q
     _deviceId = await DeviceId.getID;
     debugPrint('deviceId => $_deviceId');
     return _deviceId;
