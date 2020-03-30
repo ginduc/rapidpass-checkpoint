d8b5d07e34582ad39a294a53789d34789e206fb5
Alistair A. Israel <aisrael@gmail.com>
2020-03-30 23:28:35 +0800

    Added a test case

--- test/utils/ski32_test.dart
+++ test/utils/ski32_test.dart
@@ -5,7 +5,7 @@ import 'package:test/test.dart';
 void main() {
   test('Skip32.encrypt() works', () {
     final key = hex.decode('112233445566778899aa');
-    final testCases = {1: 3030117256};
+    final testCases = {1: 0xb49beb88, 0xffffffff: 0xbc24cc64};
     testCases.forEach((plainText, cipherText) {
       expect(Skip32.encrypt(plainText, key), equals(cipherText));
       expect(Skip32.decrypt(cipherText, key), equals(plainText));
