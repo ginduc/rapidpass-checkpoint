353371a86c0c97c31c6f1e32cb5f9542fd723cf0
Jan Salvador Sebastian <sebastian@mclinica.com>
2020-03-31 17:50:36 +0800

    Added Vibration

--- lib/screens/scan_result_screen.dart
+++ lib/screens/scan_result_screen.dart
@@ -5,6 +5,7 @@ import 'package:rapidpass_checkpoint/components/pass_results_card.dart';
 import 'package:rapidpass_checkpoint/models/scan_results.dart';
 import 'package:rapidpass_checkpoint/screens/main_menu.dart';
 import 'package:rapidpass_checkpoint/themes/default.dart';
+import 'package:vibration/vibration.dart';
 
 const borderRadius = 12.0;
 
@@ -91,6 +92,7 @@ class ScanResultScreen extends StatelessWidget {
       playNotificationApproved();
     } else {
       playNotificationRegected();
+      Vibration.vibrate(duration: 1500);
     }
 
     final card = scanResults.isValid()
@@ -134,25 +136,28 @@ class ScanResultScreen extends StatelessWidget {
                         ),
                       ),
                     ),
-                     if (scanResults.isValid() == true) 
-                       Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 8.0, horizontal: 20.0),
-                        child: SizedBox(
-                          height: 58,
-                          width: 300.0,
-                          child: RaisedButton(
-                            shape: new RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(34.0)),
-                            onPressed: () =>   Navigator.pushNamed(context, "/viewMoreInfo"),
-                            child: Text('View more info',
-                                style: TextStyle(
-                                    // Not sure how to get rid of color: Colors.white here
-                                    color: Colors.white,
-                                    fontSize: 16.0)),
-                          ),
-                        ),
-                      ),
+
+                    /// TODO : Causing error here
+
+                    //  if (scanResults.isValid() == true)
+                    //    Padding(
+                    //     padding: const EdgeInsets.symmetric(
+                    //         vertical: 8.0, horizontal: 20.0),
+                    //     child: SizedBox(
+                    //       height: 58,
+                    //       width: 300.0,
+                    //       child: RaisedButton(
+                    //         shape: new RoundedRectangleBorder(
+                    //             borderRadius: BorderRadius.circular(34.0)),
+                    //         onPressed: () =>   Navigator.pushNamed(context, "/viewMoreInfo"),
+                    //         child: Text('View more info',
+                    //             style: TextStyle(
+                    //                 // Not sure how to get rid of color: Colors.white here
+                    //                 color: Colors.white,
+                    //                 fontSize: 16.0)),
+                    //       ),
+                    //     ),
+                    //   ),
                     Padding(
                       padding: const EdgeInsets.symmetric(
                           vertical: 8.0, horizontal: 20.0),
