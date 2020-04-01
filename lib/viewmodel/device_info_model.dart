5479f132ca4a0d517acfb783577920467d07258c
Josef Solon <josefsolon@gmail.com>
2020-04-01 03:14:39 +0000

    Bug/imeicrash

--- lib/viewmodel/device_info_model.dart
+++ lib/viewmodel/device_info_model.dart
@@ -1,12 +1,30 @@
 import 'package:device_id/device_id.dart';
 import 'package:flutter/material.dart';
 import 'package:imei_plugin/imei_plugin.dart';
+import 'package:flutter/services.dart';
 
 class DeviceInfoModel extends ChangeNotifier {
   String _imei;
   String _deviceId;
+  String platformImei;
+ 
+  DeviceInfoModel() {
+    Future<String> platformImei = initPermission();
+    debugPrint('initialized imei => $_imei');
+  }
+  
+  Future<String> initPermission() async {
+     try {
+      platformImei = await ImeiPlugin.getImei( shouldShowRequestPermissionRationale: false );
+
+    } on PlatformException {
+      platformImei = 'Failed to get platform version.';
+    }
+      return platformImei;
+  }
 
   Future<String> getImei() async {
+   
     _imei = await ImeiPlugin.getImei();
     debugPrint('imei => $_imei');
     return _imei;
