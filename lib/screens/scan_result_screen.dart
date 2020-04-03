5635521ab80e4635064310511460749466abc52b
Jonel Dominic Tapang <dneljo@yahoo.com>
2020-04-03 00:53:27 +0000

    Make "Scan another QR code" button larger and rest of buttons consistent with Figma

--- lib/screens/scan_result_screen.dart
+++ lib/screens/scan_result_screen.dart
@@ -93,92 +93,100 @@ class _ScanResultScreenState extends State<ScanResultScreen> {
     return Theme(
       data: Green.buildFor(context),
       child: Scaffold(
-          appBar: AppBar(title: Text('Result')),
-          body: SingleChildScrollView(
-              child: Center(
-                child: Column(
+        appBar: AppBar(title: Text('Result')),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 20.0, vertical: 10.0),
+                  child: card,
+                ),
+                const SizedBox(height: 5),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 20.0, vertical: 10.0),
-                      child: card,
-                    ),
-                    const Padding(padding: EdgeInsets.only(top: 10.0)),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: <Widget>[
-                        if (widget.scanResults.isValid() == true)
-                          InkWell(
-                            onTap: () =>
+                    if (widget.scanResults.isValid() == true)
+                      InkWell(
+                        onTap: () =>
+                            Navigator.pushNamed(context, '/viewMoreInfo'),
+                        child: Container(
+                          padding: EdgeInsets.symmetric(
+                              vertical: 13.0, horizontal: 20.0),
+                          child: OutlineButton(
+                            borderSide: BorderSide(color: green300),
+                            highlightedBorderColor: green300,
+                            focusColor: green300,
+                            child: Text('View More Information',
+                                style: TextStyle(fontSize: 16)),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                            ),
+                            color: green300,
+                            textColor: green300,
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 16.0,
+                              vertical: 20.0,
+                            ),
+                            onPressed: () =>
                                 Navigator.pushNamed(context, '/viewMoreInfo'),
-                            child: Container(
-                              padding: EdgeInsets.symmetric(
-                                  vertical: 8.0, horizontal: 20.0),
-                              child: RaisedButton(
-                                child: Text('View more info',
-                                    style: TextStyle(fontSize: 16)),
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(34.0),
-                                ),
-                                color: green300,
-                                textColor: Colors.white,
-                                padding: EdgeInsets.symmetric(
-                                    horizontal: 16.0, vertical: 20.0),
-                                onPressed: () =>
-                                    Navigator.pushNamed(context, '/viewMoreInfo'),
-                              ),
-                            ),
-                          ),
-                        if (widget.scanResults.isValid() == true)
-                          Padding(padding: EdgeInsets.only(top: 16.0)),
-                        InkWell(
-                          onTap: () => _scanAndNavigate(context),
-                          child: Container(
-                            padding: EdgeInsets.symmetric(
-                                vertical: 8.0, horizontal: 20.0),
-                            child: RaisedButton(
-                              child: Text('Scan another QR code',
-                                  style: TextStyle(fontSize: 16)),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(34.0),
-                              ),
-                              color: green300,
-                              textColor: Colors.white,
-                              padding: EdgeInsets.symmetric(
-                                  horizontal: 16.0, vertical: 20.0),
-                              onPressed: () => _scanAndNavigate(context),
-                            ),
                           ),
                         ),
-                        InkWell(
-                          onTap: () => Navigator.popUntil(
+                      ),
+                    if (widget.scanResults.isValid() == true)
+                      InkWell(
+                        onTap: () => _scanAndNavigate(context),
+                        child: Container(
+                          padding: EdgeInsets.symmetric(
+                              vertical: 15.0, horizontal: 20.0),
+                          child: FlatButton(
+                            child: Text('Scan another QR',
+                                style: TextStyle(fontSize: 16)),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25.0),
+                            ),
+                            color: green300,
+                            textColor: Colors.white,
+                            padding: EdgeInsets.symmetric(vertical: 35.0),
+                            onPressed: () => _scanAndNavigate(context),
+                          ),
+                        ),
+                      ),
+                    InkWell(
+                      onTap: () => Navigator.popUntil(
+                          context, ModalRoute.withName('/menu')),
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                            vertical: 13.0, horizontal: 20.0),
+                        child: OutlineButton(
+                          borderSide: BorderSide(color: green300),
+                          highlightedBorderColor: green300,
+                          focusColor: green300,
+                          child: Text('Return to checker page',
+                              style: TextStyle(fontSize: 16)),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                          color: green300,
+                          textColor: green300,
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 16.0,
+                            vertical: 20.0,
+                          ),
+                          onPressed: () => Navigator.popUntil(
                               context, ModalRoute.withName('/menu')),
-                          child: Container(
-                            padding: EdgeInsets.symmetric(
-                                vertical: 8.0, horizontal: 20.0),
-                            child: OutlineButton(
-                              borderSide: BorderSide(color: green300),
-                              focusColor: green300,
-                              child: Text('Return to checker page',
-                                  style: TextStyle(fontSize: 16)),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(34.0),
-                              ),
-                              color: green300,
-                              textColor: green300,
-                              highlightedBorderColor: green300,
-                              padding: EdgeInsets.symmetric(
-                                  horizontal: 16.0, vertical: 20.0),
-                              onPressed: () => Navigator.popUntil(
-                                  context, ModalRoute.withName('/menu')),
-                            ),
-                          ),
                         ),
-                      ],
+                      ),
                     ),
                   ],
                 ),
-          ))),
+                const SizedBox(height: 30),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 
