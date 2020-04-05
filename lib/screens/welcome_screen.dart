90815fe53f33262689abeca1fd1aa9077ed375a0
Vince Ramces Oliveros <blusea231@gmail.com>
2020-04-05 23:06:37 +0800

    dart formatter

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 import 'package:rapidpass_checkpoint/common/constants/rapid_asset_constants.dart';
+import 'package:rapidpass_checkpoint/common/constants/rapid_route_constants.dart';
 import 'package:rapidpass_checkpoint/screens/credits_screen.dart';
 import 'package:rapidpass_checkpoint/themes/default.dart';
 import 'package:rapidpass_checkpoint/viewmodel/device_info_model.dart';
@@ -102,12 +103,12 @@ class WelcomeScreenState extends State<WelcomeScreen>
                             shape: new RoundedRectangleBorder(
                                 borderRadius: BorderRadius.circular(24.0)),
                             onPressed: () {
-                              Navigator.pushNamed(context, "/menu");
+                              Navigator.pushNamed(
+                                  context, RapidPassRoutes.MENU);
                             },
                             child: Text("Start",
                                 style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 18.0)),
+                                    color: Colors.white, fontSize: 18.0)),
                           ),
                         ),
                       ),
@@ -144,7 +145,8 @@ class WelcomeScreenState extends State<WelcomeScreen>
 
   Widget _buildFooter(String title) {
     return GestureDetector(
-      onTap: () => Navigator.pushNamed(context, "/${title.replaceAll(' ', '_').toLowerCase()}"),
+      onTap: () => Navigator.pushNamed(
+          context, "/${title.replaceAll(' ', '_').toLowerCase()}"),
       child: Text(
         title,
         style: TextStyle(
