96c21db240fcd5f747fdaec59584e90148cf94e8
Ariel De Castro <ariel.dcastro@gmail.com>
2020-04-13 08:10:25 +0800

    Prepare helper functions on App State.

--- lib/models/app_state.dart
+++ lib/models/app_state.dart
@@ -10,12 +10,18 @@ class AppState extends ChangeNotifier {
   String _masterQrCode;
   AppSecrets _appSecrets;
   PackageInfo _packageInfo;
+  dynamic _databaseSyncLog = [];
+  int _databaseRecordCount = 0;
 
   static final DateFormat dateFormat = DateFormat.yMMMd('en_US').add_jm();
 
   String get databaseLastUpdatedText => (_databaseLastUpdated != null)
       ? 'Database last updated on ${dateFormat.format(_databaseLastUpdated)}'
       : null;
+
+  int get databaseLastUpdated => _databaseLastUpdated != null
+      ? _databaseLastUpdated.millisecondsSinceEpoch
+      : 0;
 
   set databaseLastUpdated(final int timestamp) {
     debugPrint('databaseLastUpdated($timestamp)');
@@ -25,13 +31,30 @@ class AppState extends ChangeNotifier {
     notifyListeners();
   }
 
+  dynamic get databaseSyncLog => _databaseSyncLog;
+
+  dynamic addDatabaseSyncLog(Object data) {
+    this._databaseSyncLog.add(data);
+    notifyListeners();
+    return data;
+  }
+
+  int get databaseRecordCount => _databaseRecordCount;
+
+  set databaseRecordCount(int value) {
+    this._databaseRecordCount = value;
+    notifyListeners();
+  }
+
   String get masterQrCode => this._masterQrCode;
+
   set masterQrCode(final String masterQrCode) {
     this._masterQrCode = masterQrCode;
     notifyListeners();
   }
 
   AppSecrets get appSecrets => this._appSecrets;
+
   Future<AppSecrets> setAppSecrets(final AppSecrets appSecrets) {
     this._appSecrets = appSecrets;
     return AppStorage.setAppSecrets(appSecrets).then((secrets) {
@@ -41,6 +64,7 @@ class AppState extends ChangeNotifier {
   }
 
   PackageInfo get packageInfo => this._packageInfo;
+
   set packageInfo(final PackageInfo packageInfo) {
     this._packageInfo = packageInfo;
     notifyListeners();
