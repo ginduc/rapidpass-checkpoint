c3bb9d64e14d961ce2416e3d6a332f773f49a20e
Atik <atik@live.com.ph>
2020-04-24 02:17:28 +0800

    replace imei with android id

--- lib/viewmodel/device_info_model.dart
+++ lib/viewmodel/device_info_model.dart
@@ -1,38 +1,28 @@
-import 'package:device_id/device_id.dart';
+import 'package:device_info/device_info.dart';
 import 'package:flutter/material.dart';
-import 'package:imei_plugin/imei_plugin.dart';
-import 'package:flutter/services.dart';
 
 class DeviceInfoModel extends ChangeNotifier {
   String _imei;
   String get imei => _imei;
   String _deviceId;
-  String platformImei;
 
   DeviceInfoModel() {
-    // Future<String> platformImei = initPermission();
     debugPrint('initialized imei => $_imei');
   }
 
-  Future<String> initPermission() async {
-    try {
-      platformImei =
-          await ImeiPlugin.getImei(shouldShowRequestPermissionRationale: false);
-    } on PlatformException {
-      platformImei = 'Failed to get platform version.';
-    }
-    return platformImei;
-  }
-
   Future<String> getImei() async {
-    _imei = await ImeiPlugin.getImei();
+    _imei = await getDeviceId();
     debugPrint('imei => $_imei');
     notifyListeners();
     return _imei;
   }
 
   Future<String> getDeviceId() async {
-    _deviceId = await DeviceId.getID;
+    AndroidDeviceInfo androidInfo = await DeviceInfoPlugin().androidInfo;
+    debugPrint(
+        'Device Manufacturer ${androidInfo.manufacturer}, Model ${androidInfo.brand} ${androidInfo.model}');
+
+    _deviceId = androidInfo.androidId.toUpperCase();
     debugPrint('deviceId => $_deviceId');
     return _deviceId;
   }
